5835f3af88893be8f894cd1ac35222366a980715
Jeremy Bannister <jeremy.a.bannister@gmail.com>
2022-07-22 23:21:30 +0200

    Initial Commit

---
+++ Sources/PasteboardModule/PasteboardModule.swift
@@ -0,0 +1,6 @@
+public struct PasteboardModule {
+    public private(set) var text = "Hello, World!"
+
+    public init() {
+    }
+}
---
+++ Tests/PasteboardModuleTests/PasteboardModuleTests.swift
@@ -0,0 +1,11 @@
+import XCTest
+@testable import PasteboardModule
+
+final class PasteboardModuleTests: XCTestCase {
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct
+        // results.
+        XCTAssertEqual(PasteboardModule().text, "Hello, World!")
+    }
+}
